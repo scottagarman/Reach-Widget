3aeed65b7d8a1a7f29ddf3ac8372a4f31561ac85
Ryan Thomas <neonxero@gmail.com>
2010-11-06 00:42:06 -0400

    Cleaned up, commented stuff, got widget working - no click yet... and... I'm taking a break I guess

---
+++ src/com/neonlotus/android/reach/Widget.java
@@ -0,0 +1,7 @@
+package com.neonlotus.android.reach;
+
+import android.appwidget.AppWidgetProvider;
+
+public class Widget	extends AppWidgetProvider { }
+
+//Not much to this one, just setting it up for a provider I guess

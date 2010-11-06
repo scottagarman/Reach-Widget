ccb31c543e4fa9bfab50dd1fd0e491afb32388f5
Scott A Garman <sag5066@gmail.com>
2010-11-05 21:12:15 -0400

    Added test for player stats.

--- src/com/neonlotus/android/reach/ReachMain.java
+++ src/com/neonlotus/android/reach/ReachMain.java
@@ -1,15 +1,34 @@
 package com.neonlotus.android.reach;
 
+import org.json.JSONObject;
+
+import com.neonlotus.android.reach.controller.JsonParserController;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ReachMain extends Activity {
-    /** Called when the activity is first created. */
-    @Override
+    private static final String DEBUG_TAG = "ReachWidget/ReachMain";
+
+    
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
         //Banging comments
+        
+        
+        JSONObject stats;
+        
+        JsonParserController jpc = new JsonParserController();
+        //do this in new thread duh
+        stats = jpc.parse("http://www.bungie.net/api/reach/reachapijson.svc/player/details/nostats/DANs$7-WyOGpTthopASqbsJE96sMV0mKCGv6$FDm$7k=/fr0z3nph03n1x");
+        
+        if(stats != null){
+        	Log.d(DEBUG_TAG, "Results!: " + stats.toString());
+        }else{
+        	Log.d(DEBUG_TAG, "Failed!" );
+        }
     }
 }

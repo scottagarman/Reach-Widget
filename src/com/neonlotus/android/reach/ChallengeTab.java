c4078aee37a188c2825a0ec151ec78c7ac2b68ec
Ryan Thomas <neonxero@gmail.com>
2010-11-05 21:05:44 -0400

    Made the launch page

---
+++ src/com/neonlotus/android/reach/ChallengeTab.java
@@ -0,0 +1,15 @@
+package com.neonlotus.android.reach;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class ChallengeTab extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.challengetab);
+        
+    }
+}
--- src/com/neonlotus/android/reach/ReachMain.java
+++ src/com/neonlotus/android/reach/ReachMain.java
@@ -1,15 +1,44 @@
 package com.neonlotus.android.reach;
 
-import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-public class ReachMain extends Activity {
+public class ReachMain extends TabActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        //Banging comments
+        //Banging comments "Bangin comments" FTFY
+        Resources res = getResources(); // Resource object to get drawables
+        TabHost tabHost = getTabHost(); // the activity tab host
+        TabHost.TabSpec spec; // reusable tabspec for each tab
+        Intent intent; // reusable intent for each tab
+        
+        //Create an Intent to launch an activity for th etab (to be reused)
+        intent = new Intent().setClass(this, ChallengeTab.class);
+        
+        //Initialize a TabSpec for each tab and add it to the TabHost
+        spec = tabHost.newTabSpec("main").setIndicator("Main", res.getDrawable(R.drawable.ic_tab_main)).setContent(intent);
+        tabHost.addTab(spec);
+        
+        //Other tabs
+        intent = new Intent().setClass(this, ChallengeTab.class);
+        spec = tabHost.newTabSpec("fav").setIndicator("Challenges", res.getDrawable(R.drawable.ic_tab_fav)).setContent(intent);
+        tabHost.addTab(spec);
+        
+        intent = new Intent().setClass(this, ChallengeTab.class);
+        spec = tabHost.newTabSpec("about").setIndicator("Friends", res.getDrawable(R.drawable.ic_tab_about)).setContent(intent);
+        tabHost.addTab(spec);
+        
+        intent = new Intent().setClass(this, ChallengeTab.class);
+        spec = tabHost.newTabSpec("settings").setIndicator("Settings", res.getDrawable(R.drawable.ic_tab_about)).setContent(intent);
+        tabHost.addTab(spec);
+        
+        tabHost.setCurrentTab(0);
     }
 }

b1b31fb8eca62529601015a8ad36e90002e1a6f8
Scott A Garman <sag5066@gmail.com>
2010-11-14 01:42:22 -0500

    Finished almost all stub code for homescreen widget. Widget part is ok, configure activity needs to be fully thoughtout and implemented.

--- src/com/neonlotus/android/reach/widget/Widget.java
+++ src/com/neonlotus/android/reach/widget/Widget.java
@@ -13,6 +13,7 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.graphics.Bitmap;
 import android.os.IBinder;
 import android.util.Log;
@@ -30,34 +31,20 @@ public class Widget	extends AppWidgetProvider {
 		updateIntent.putExtra(REACHCONFIG.Intents.EXTRA_IDS, appWidgetIds);
 		
 		// Start update server
-		context.startService(updateIntent);
-		
-		/*
-		final int N = appWidgetIds.length;
-		
-		    // Perform this loop procedure for each App Widget that belongs to this provider
-		for (int i=0; i<N; i++) {
-			int appWidgetId = appWidgetIds[i];
-			
-			// Create an Intent to launch ExampleActivity
-			Intent intent = new Intent(context, ExampleActivity.class);
-			PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-			
-			// Get the layout for the App Widget and attach an on-click listener to the button
-			RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.appwidget_provider_layout);
-			views.setOnClickPendingIntent(R.id.button, pendingIntent);
-			
-			// Tell the AppWidgetManager to perform an update on the current App Widget
-			appWidgetManager.updateAppWidget(appWidgetId, views);
-		}
-		*/
-		
+		context.startService(updateIntent);		
 	} 
 	
 	public void onDeleted(Context context, int[] appWidgetIds) {
 		super.onDeleted(context, appWidgetIds);
+		SharedPreferences sp = context.getSharedPreferences(REACHCONFIG.Preferences.ALL, Context.MODE_PRIVATE);
+		Editor editor = sp.edit();
+		String tag;
 		
-		//remove pref
+		for(int appWidgetId : appWidgetIds){
+			tag = sp.getString(REACHCONFIG.Preferences.TAG_KEY + appWidgetId, null);
+			editor.remove(REACHCONFIG.Preferences.TAG_KEY + appWidgetId);
+		}
+		editor.commit();
 	}
 	
 	/**
---
+++ src/com/neonlotus/android/reach/widget/WidgetConfigure.java
@@ -0,0 +1,103 @@
+package com.neonlotus.android.reach.widget;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import org.json.JSONObject;
+
+import com.neonlotus.android.reach.R;
+import com.neonlotus.android.reach.REACHCONFIG;
+import com.neonlotus.android.reach.controller.ImageFetcherController;
+import com.neonlotus.android.reach.controller.JsonParserController;
+import com.neonlotus.android.reach.model.Player;
+
+import android.app.Activity;
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RemoteViews;
+
+public class WidgetConfigure extends Activity implements OnClickListener{
+	private EditText editText;
+	private Button button;
+	
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.configure_layout_stub);
+        
+        editText = (EditText) findViewById(R.id.configureEditText);
+        button = (Button) findViewById(R.id.configureButton);
+        
+        button.setOnClickListener(this);
+    }
+
+    public void setWidget(){
+    	String value;
+    	JSONObject stats = null;
+    	value = editText.getText().toString();
+    	
+    	JsonParserController jpc = new JsonParserController();
+    	try {
+    		stats = jpc.parse("http://www.bungie.net/api/reach/reachapijson.svc/player/details/nostats/DANs$7-WyOGpTthopASqbsJE96sMV0mKCGv6$FDm$7k=/" 
+					+ URLEncoder.encode(value.trim(),"utf-8"));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		if(stats != null){
+			Player p = new Player(stats);
+			ImageFetcherController ifc = new ImageFetcherController();
+			Bitmap image = ifc.getImageFromUrl("http://www.bungie.net/" + p.playerModelUrl);
+			if(image != null){
+				updateWidget(this, p.playerModelUrl, image);
+			}
+		}
+		
+    	
+    }
+    
+	public void updateWidget(Context context, String url, Bitmap image){
+		int appWidgetId = 0;
+		Intent intent = getIntent();
+		Bundle extras = intent.getExtras();
+		if (extras != null) {
+		    appWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+		}
+		
+		SharedPreferences sp = getSharedPreferences(REACHCONFIG.Preferences.ALL, MODE_PRIVATE);
+		Editor editor = sp.edit();
+		editor.putString(REACHCONFIG.Preferences.TAG_KEY+appWidgetId, url);
+		editor.commit();
+		
+		//tempTag = sp.getString(REACHCONFIG.Preferences.TAG_KEY + appWidgetId, null);
+		
+		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+		RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.w_main);
+		views.setImageViewBitmap(R.id.widgetavatar, image);
+
+		appWidgetManager.updateAppWidget(appWidgetId, views);
+		Intent resultValue = new Intent();
+		resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+		setResult(RESULT_OK, resultValue);
+		finish();			
+	}
+    
+    
+    
+	public void onClick(View v) {
+		switch(v.getId()){
+			case R.id.configureButton:
+				setWidget();
+				break;
+		}
+		
+	}
+}
